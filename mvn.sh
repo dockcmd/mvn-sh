6f53f4db94c412d5ca618db92aabea8a818d8136
Sean Drucker <srdrucker@gmail.com>
2020-05-21 19:30:07 -0500

    m2 cache directory creation

--- mvn.sh
+++ mvn.sh
@@ -3,6 +3,12 @@
 
 include github.com/dockcmd/shmod util.sh
 
+# ensure .m2 directory created with current user
+if ! [ -d "$HOME/.m2" ] 
+then
+  mkdir $HOME/.m2
+fi
+
 # Maven call via docker
 exec_or_dryrun \
   $(docker_run) \
